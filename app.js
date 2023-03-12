6775b071446287d2be6c64bf1738e50b16f82140
Chilka <nripayena.n.chilka@accenture.com>
2023-03-13 00:53:23 +0530

    Html changes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post('/signup', function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000,function(){
-    console.log("Server is running on port 3000.")
+    console.log("Server is running .")
 })
 
 //API Key
